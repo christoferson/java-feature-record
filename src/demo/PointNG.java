c0afe6243f93abb65f98a04b5349556a6f145bc3
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-10-11 14:29:19 +0900

    Record implicitly final

--- src/demo/PointNG.java
+++ src/demo/PointNG.java
@@ -1,10 +1,14 @@
 package demo;
 
-public record PointNG(int x, int y) {
+public record PointNG(int x, int y) { 
 	
 // A record class declaration does not have an extends clause. The superclass of a record class is always java.lang.Record
 // Even though a normal class can explicitly extend its implicit superclass Object, a record cannot explicitly extend any class, even its implicit superclass Record.
 //public record PointNG(int x, int y) extends java.lang.Record { //Syntax error on token "extends", implements expected
 
-
+// A record class is implicitly final, and cannot be abstract. 
+// These restrictions emphasize that the API of a record class is defined solely by its state description, and cannot be enhanced later by another class.
+//public abstract record PointNG(int x, int y) { // Illegal modifier for the record PointNG; only public, final and strictfp are permitted
+		
+	
 }
